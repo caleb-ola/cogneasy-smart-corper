def4d506f7a42cad1fc04737a25e1b9031d9d9be
Your Name <olajiire2@gmail.com>
2021-02-17 11:09:58 +0100

    absolute side images

--- src/components/schedule-section/schedule.js
+++ src/components/schedule-section/schedule.js
@@ -8,13 +8,11 @@ import Focus from "../focus";
 const Schedule = () => {
   return (
     <div className="schedule">
-      <Bounce right duration={2000}>
-        <img
-          className="vector-one"
-          src="/images/sidelogo1.png"
-          alt="cogneasy-logo"
-        />
-      </Bounce>
+      <img
+        className="vector-one"
+        src="/images/sidelogoa.png"
+        alt="cogneasy-logo"
+      />
 
       <div className="register-header">
         <h1 className="speakers-header">
@@ -23,13 +21,12 @@ const Schedule = () => {
         </h1>
         <p className="speakers-text">Schedule and Agenda for Smart Corpers .</p>
       </div>
-      <Bounce left duration={3000}>
-        <img
-          className="vector-two"
-          src="/images/sidelogo2.png"
-          alt="cogneasy-logo"
-        />
-      </Bounce>
+
+      <img
+        className="vector-two"
+        src="/images/sidelogob.png"
+        alt="cogneasy-logo"
+      />
 
       <div className="schedule-body">
         <Focus />
